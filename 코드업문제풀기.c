6cbfa994622d9028535e09b1a31af61b550d3e85
HADONGGUN <163298112+dongguli08@users.noreply.github.com>
2024-04-04 08:16:06 +0900

    Add files via upload

---
+++ 코드업문제풀기.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+
+/*int main(void) {
+	float x;
+	scanf("%f", &x);
+	printf("%.2f", x);
+
+    return 0;
+}*/
+
+/*#include <stdio.h>
+
+int main(void){
+	int x;
+	scanf("%d",&x);
+	printf("%d %d %d",x,x,x);
+}*/
+
+/*#include <stdio.h>
+
+int main(void){
+	int h,m;
+	scanf("%d:%d",&h,&m);
+	printf("%d:%d",h,m);
+}*/
+
+/*#include <stdio.h>
+
+int main(void){
+	int year,month,day;
+	scanf(%d,%d,%d,&year,&month,&day);
+	printf("%04d %02d %02d",year,month,day);
+}*/
+
+#include <stdio.h>
+
+/*int main(void){
+	int f,b;
+	scanf("%d-%d",&f,&b);
+	printf("%d %d",f,b);
+	}*/
+	
+/*#include  <stdio.h>
+
+int main(void){
+	char data[51]
+	scanf("%s",&data);
+	printf("%s",data);
+}*/
+
+
+/*#include <stdio.h>
+
+int main(void){
+
+char d[21]; 
+scanf("%s", d); 
+for(int i=0; d[i]!='\0'; i++) 
+{
+   printf("\'%c\'\n", d[i]);
+}*/
+
+ /*#include<stdio.h>
+
+int main(void){
+	int a,b,c,d,e;
+	scanf("%1d%1d%1d%1d%1d",&a,&b,&c,&d,&e);
+    printf("\[%d]\n [%d]\n [%d]\n [%d]\n [%d]",a*10000,b*1000,c*100,d*10,e);
+	return 0;
+}*/
+/*#include <stdio.h>
+
+int main(void){
+	int h, m, s;
+	scanf("%d:%d:%d", &h, &m, &s);
+	printf("%d",m);
+}*/
+
+#include <stdio.h>
+
+/*int main(void){
+	int y,m,d;
+	scanf("%04d %02d %02d",y,m,d);
+	printf("%02d-%02d-%04d",d,m,y);
+	return 0;
+}*/
