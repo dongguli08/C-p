d042af42331fc63539445819fd3182b070b3f7b2
HADONGGUN <163298112+dongguli08@users.noreply.github.com>
2024-04-16 11:30:20 +0900

    Add files via upload

--- FileName.c
+++ FileName.c
@@ -1,33 +1,67 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
+#include<stdio.h>
 
-// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
-char* solution(const char* rny_string) {
+int main(void) {
 
-    char* answer = (char*)malloc(strlen(rny_string) * 2 + 1);
-    int len = strlen(rny_string);
+	/*char string1[6] = {'H','e','l','l','o','\0'};//널문자를(항상 +1) 넣지 않으면 끝을 알수없다
+	char string2[6] = "wprld";
+	printf("%s\n", string1);
+	printf("%s\n", string2);
+	printf("%s %s\n", string1, string2);*/
 
-    for (int i = 0; i < len; i++) {
-        if (rny_string[i] == 'm') len++;
-    }
 
-    int j = 0;
-    int k = 0;
-    while (rny_string[j] != '\0') {
-        if (rny_string[j] == 'm') {
-            answer[k] = 'r';
-            k++;
-            answer[k] = 'n';
-            k++;
-            j++;
-        }
-        else {
-            answer[k] = rny_string[j];
-            k++;
-            j++;
-        }
-    }
-    answer[k] = '\0';
-    return answer;
+
+	/*char str[18] = "Nice to meet you!";
+	printf("배열 str크기 :%d\n", sizeof(str));
+	printf("Null 문자 문자형 출력:%c\n", str[17]);
+	printf("Null 문자 문자형 출력:%d\n", str[17]);
+	str[16] = '?';
+	printf("문자열 출력 : %s\n", str);
+
+	return 0;*/
+
+	/*int cnt = 0; // 이름의 길이를 세는 변수
+	char sName[100], sNum[10]; //이름과 학번을 문자열로 저장하는 배열
+	printf("이름을 영어로 입력하세요:");
+	scanf_s("%s", sName); //이름을 문자열로 입력받기
+	printf("학번을 입력하세요:");
+	//학번 입력받기
+	while (sName[cnt] != '\0') {
+		cnt++;
+		
+	}
+	printf("당신의 이름은 %s이고 학번은 %d 입니다", sName, sNum);
+
+	return 0;*/
+
+	int numbers[11]; // numbers 10개의 공간을 저장
+	int i;
+	int average=0;
+	int min=100;
+	int max = 0;
+	
+	printf("10개의 숫자를 입력하세요:\n");
+	for (i = 0; i < 10; i++) {
+		printf("숫자 %d: ", i + 1);
+		scanf_s("%d", &numbers[i]);
+	}
+
+	for (int i = 0; i < 10; i++) {
+		average += numbers[i];
+		}
+	printf("평균값은 %d\n", average / 10);
+
+	for (int i = 0; i < 10; i++) {
+		if (numbers[i] < min)
+			min = numbers[i];// numbers[j]에 값들이 저장되어 있음 0부터 9까지 늘어나면서 값들을 비교해서 최저값을 구함
+	}
+	printf("제일 작은 값은%d\n", min);
+
+	for (int i = 0; i < 10; i++) {
+		if (numbers[i] > max)
+			max = numbers[i]; 
+	}
+	printf("제일 큰 값은%d", max); 
+	//성적표 최댓값 최솟값 평균값
+
+	
 }
