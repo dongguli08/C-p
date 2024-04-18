168496eb6752c8b6a9b00722bb7efbf4e6253889
HADONGGUN <163298112+dongguli08@users.noreply.github.com>
2024-04-18 16:37:45 +0900

    Add files via upload

---
+++ 04-18프로그래밍 수업.cpp
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+int main(void){
+
+/*int max,min,temp,sum;
+
+printf("큰 수와 작은 수를 입력하시오:");
+scanf("%d %d",&max,&min ); 
+
+	if(max>min){
+	
+	for(int i=min; i<=max; i++){
+
+	sum+=i; //sum에다가 두사이 정수들의 합을 넣어줌 
+	printf("%d ",i);// 두 정수 사이의 정수들을 출력 
+   }
+   printf("\n");
+   printf("두개의 정수 사이에 존재하는 정수들의 합은 %d",sum);	
+ } 
+ else
+ printf("잘못된 입력입니다");*/
+ 
+ 
+ 
+ for(int i=1; i<11 ; i++){
+ 	if(i%2==0)continue;
+ 	printf("%d",i);
+ } 
+ 
+ 
+}
+
+
+
+
+
+
+
+
+
